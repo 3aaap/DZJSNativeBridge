cfc7c13e9ac378ed22663bbd42d6ed0c1a16198e
songdezhong <songdezhong@58ganji.com>
2016-02-29 14:53:12 +0800

    使用拦截自定义协议实现 js 与 native 互调

--- DZJSNativeBridge/DZJSNativeBridge/MainViewController.m
+++ DZJSNativeBridge/DZJSNativeBridge/MainViewController.m
@@ -8,7 +8,9 @@
 
 #import "MainViewController.h"
 
-@interface MainViewController ()
+@interface MainViewController () <UIWebViewDelegate>
+
+@property (nonatomic, weak) UIWebView* webView;
 
 @end
 
@@ -17,6 +19,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    UIWebView* webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    self.webView = webView;
+    self.webView.delegate = self;
+    [self.view addSubview:webView];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"TestJSBridge__tradition.html" ofType:nil];
+    NSString* HTMLStr = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
+    [self.webView loadHTMLString:HTMLStr baseURL:nil];
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+#pragma mark----- UIWebViewDelegate -----
+
+- (BOOL)webView:(UIWebView*)webView shouldStartLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    if ([request.URL.scheme isEqualToString:@"dzbridge"]) {
+        NSArray<NSString*>* arr = [request.URL.absoluteString componentsSeparatedByString:@"?"];
+        if (arr.count > 1) {
+            NSString* str = [arr[1] stringByRemovingPercentEncoding];
+            NSDictionary* dict = [NSJSONSerialization JSONObjectWithData:[str dataUsingEncoding:NSUTF8StringEncoding] options:0 error:NULL];
+            NSLog(@"%@", dict[@"string"]);
+        } else{
+            NSLog(@"没有参数的打印");
+        }
+        return NO;
+    }
+
+    return YES;
 }
 
 @end
