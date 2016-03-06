204ca0b3971ba10f375ae2bef709784cdc0c6099
songdezhong <songdezhong@58ganji.com>
2016-03-06 11:36:26 +0800

    添加注释和打印

--- DZJSNativeBridge/DZJSNativeBridge/MainViewController.m
+++ DZJSNativeBridge/DZJSNativeBridge/MainViewController.m
@@ -43,19 +43,28 @@
 
 - (BOOL)webView:(UIWebView*)webView shouldStartLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
 {
+    //dzbridge 为约定好的协议头，如果是，则页面不进行跳转
     if ([request.URL.scheme isEqualToString:@"dzbridge"]) {
+        //截取字符串来判断是否存在参数
         NSArray<NSString*>* arr = [request.URL.absoluteString componentsSeparatedByString:@"?"];
         if (arr.count > 1) {
             NSString* str = [arr[1] stringByRemovingPercentEncoding];
             NSDictionary* dict = [NSJSONSerialization JSONObjectWithData:[str dataUsingEncoding:NSUTF8StringEncoding] options:0 error:NULL];
             NSLog(@"%@", dict[@"string"]);
-        } else{
+        }
+        else {
             NSLog(@"没有参数的打印");
         }
         return NO;
     }
-
+    //不是自定义协议头，跳转页面
     return YES;
+}
+
+- (void)webViewDidFinishLoad:(UIWebView*)webView
+{
+    NSString* str = [self.webView stringByEvaluatingJavaScriptFromString:@"pageDidLoad()"];
+    NSLog(@"%@", str);
 }
 
 @end
